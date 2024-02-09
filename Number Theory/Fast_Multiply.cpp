f1c124a2d5c5a5ba600d839c0f2beab2f9649ee5
hizenberg <growlingraptor@gmail.com>
2024-02-09 04:24:01 +0000

    Some Changes

--- Number Theory/Fast_Multiply.cpp
+++ Number Theory/Fast_Multiply.cpp
@@ -1,9 +1,19 @@
-int fast_multiply(int a,int b,int c){
-	int res=0;
-	while(b){
-		if(b&1)res+=a,res%=c;
-		a+=a,a%=c;
-		b>>=1;
-	}
-	return res;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int binary_multiplication(int a , int b,int M=(int)1e9+7){
+    int res = 0;
+    while( b ){
+        if( b&1 ) res += a , res %= M;
+        a = 2 * a ; a %= M:
+        b >>= 1;
+    }
+
+    return res;
+}
+
+int main(){
+
+    return 0;
 }
