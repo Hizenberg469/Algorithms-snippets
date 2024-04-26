cd58ef959f24c313bcd42d079b904e722556e463
Hizenberg <growlingraptor@gmail.com>
2024-04-26 21:21:25 +0530

    Leetcode template

---
+++ LeetcodeTemplate.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+
+public:
+
+	//Solution function template...
+	
+};
+
+int main( int argc, char* argv[] ){
+
+	Solution sol;
+
+	//solution function call...
+	return 0;
+}
